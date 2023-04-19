96071dc81ef10ad6db852d48c84e54dcc6f4704e
tvargasl <126362144+tvargasl@users.noreply.github.com>
2023-04-19 10:06:22 -0500

    Add files via upload

---
+++ Actividad3Cap4Ejercicio22/src/main/java/misproyectos/actividad3cap4ejercicio22/Actividad3Cap4Ejercicio22.java
@@ -0,0 +1,9 @@
+package misproyectos.actividad3cap4ejercicio22;
+
+public class Actividad3Cap4Ejercicio22 {
+
+    public static void main(String[] args) {
+         InterfazUsuario iu = new InterfazUsuario();
+        iu.setVisible(true);
+    }
+}
---
+++ Actividad3Cap4Ejercicio22/src/main/java/misproyectos/actividad3cap4ejercicio22/Salario.java
@@ -0,0 +1,9 @@
+package misproyectos.actividad3cap4ejercicio22;
+
+public class Salario {
+    public static double calcularSalario(double salarioHora, double horas){
+        double salario = salarioHora*horas;
+        return salario;
+        
+    }
+}
