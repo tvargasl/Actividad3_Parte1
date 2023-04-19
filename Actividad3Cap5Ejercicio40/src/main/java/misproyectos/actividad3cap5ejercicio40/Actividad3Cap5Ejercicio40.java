70f78aabcba83f8ae008ee329ca5bb62700dffd6
tvargasl <126362144+tvargasl@users.noreply.github.com>
2023-04-19 10:29:34 -0500

    Add files via upload

---
+++ Actividad3Cap5Ejercicio40/src/main/java/misproyectos/actividad3cap5ejercicio40/Actividad3Cap5Ejercicio40.java
@@ -0,0 +1,9 @@
+package misproyectos.actividad3cap5ejercicio40;
+
+public class Actividad3Cap5Ejercicio40 {
+
+    public static void main(String[] args) {
+         InterfazUsuario iu = new InterfazUsuario();
+        iu.setVisible(true);
+    }
+}
---
+++ Actividad3Cap5Ejercicio40/src/main/java/misproyectos/actividad3cap5ejercicio40/Calculos.java
@@ -0,0 +1,19 @@
+package misproyectos.actividad3cap5ejercicio40;
+
+public class Calculos {
+    public static double raiz(double numero){
+        double x = Math.sqrt(numero);
+        return x;
+    }
+    
+    public static double cuadrado(double numero){
+        double x = Math.pow(numero,2);
+        return x;
+    }
+    
+    public static double cubo(double numero){
+        double x = Math.pow(numero,3);
+        return x;
+    }
+}
+
